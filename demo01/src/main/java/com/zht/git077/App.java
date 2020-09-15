3b171a1aa85bf249aa41c83dd3916830522b2cc2
zhaohaitian <397408662@qq.com>
2020-09-15 17:09:37 +0800

    创建demo01

---
+++ demo01/src/main/java/com/zht/git077/App.java
@@ -0,0 +1,14 @@
+package com.zht.git077;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("测试");
+    }
+}
